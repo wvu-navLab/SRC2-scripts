d900b8c5d7c04266e3ecb8f8a4379c4038f6519e
Jared Strader <jared.l.strader@gmail.com>
2021-07-06 20:00:14 -0400

    Update source_cmd.sh

--- source_cmd.sh
+++ source_cmd.sh
@@ -2,6 +2,7 @@
 # mkdir ciao
 
 source /home/srcp2/ros_workspace/install/setup.bash
-source /home/srcp2/cmp_workspace/devel/setup.bash
+#source /home/srcp2/cmp_workspace/devel/setup.bash
+source /home/srcp2/cmp_workspace/install/setup.bash
 
 export ROS_MASTER_URI=http://172.18.0.2:11311
